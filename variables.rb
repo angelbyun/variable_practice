fb2b1addbdda5336edefebc16bf5bf30f30de211
Angel Byun <abyun22@turing.edu>
2023-01-03 16:28:12 -0700

    Add Booleans

--- variables.rb
+++ variables.rb
@@ -7,7 +7,12 @@ greeting = "Hello!"
 age = 33
 number of pets = 1
 
-# Float
-cost of dog food = 50.00
-cost of pet insurance = 35.00
-cost of snowboarding pass = 600.00
+# Floats
+dog_food = 50.00
+pet_insurance = 35.00
+snowboarding_pass = 600.00
+
+# Booleans
+pet_insurance_paid = true
+car_insurance_paid = true
+tuition_paid = false
